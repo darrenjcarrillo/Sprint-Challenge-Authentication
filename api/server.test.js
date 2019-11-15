9df2f31a49836345eb045b4e30b3e15b616beb55
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-11-15 11:04:47 -0800

    MVP

--- api/server.test.js
+++ api/server.test.js
@@ -3,22 +3,12 @@ const request = require("supertest");
 const server = require("./server.js");
 const db = require("../database/dbConfig.js");
 
-const authenticate = require("../auth/authenticate-middleware.js");
-const authRouter = require("../auth/auth-router.js");
-
 it("should set db environment to testing", function() {
   expect(process.env.DB_ENV).toBe("testing");
 });
 
 describe("Should get all jokes", function() {
   describe("GET /", function() {
-    // it("get all jokes should return 200 OK", function() {
-    //   return request(server)
-    //     .get("/api/jokes", authenticate, jokesRouter)
-    //     .then(res => {
-    //       expect(res.status).toBe(200);
-    //     });
-    // });
     beforeEach(async () => {
       await db("users").truncate();
     });
@@ -26,7 +16,7 @@ describe("Should get all jokes", function() {
     it("should return 201 OK", function() {
       return request(server)
         .post("/api/auth/register")
-        .send({ username: "HELLO", password: "HELLO" })
+        .send({ username: "TEST", password: "TEST" })
         .then(res => {
           expect(res.status).toBe(201);
         });
@@ -35,26 +25,24 @@ describe("Should get all jokes", function() {
     it("should return have username", function() {
       return request(server)
         .post("/api/auth/register")
-        .send({ username: "HELLO", password: "HELLO" })
+        .send({ username: "TEST", password: "TEST" })
         .then(res => {
-          expect(res.body.username).toEqual("HELLO");
+          expect(res.body.username).toEqual("TEST");
         });
     });
 
-    // it("succeeds with correct credentials", function() {
-    //   return request(server)
-    //     .post("/api/auth/login")
-    //     .send({ username: "HELLO", password: "HELLO" })
-    //     .then(res => {
-    //       expect(res.status).toBe(201);
-    //     });
-    // });
+    it("should return status 201 on login", async () => {
+      //user registers
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send({ username: "JOSHUA", password: "JOSHUA" });
 
-    it("good login", async () => {
-      try {
-        const response = await request(server.login)('"HELLO"', '"HELLO"');
-        expect(response.status).toBe(200);
-      } 
+      //user logs in
+      const loginResponse = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "JOSHUA", password: "JOSHUA" });
+      expect(loginResponse.status).toBe(200);
+    });
 
     it("Can't login with Invalid credentials", async function() {
       const response = await request(server)
